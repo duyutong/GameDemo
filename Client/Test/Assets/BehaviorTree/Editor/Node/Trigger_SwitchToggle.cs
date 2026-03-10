ef7c38e4cf513ea1a60f0d2f8e349cf78ea458ff
duyutong <729052582@qq.com>
2026-03-10 17:30:35 +0800

    -

--- Client/Test/Assets/BehaviorTree/Editor/Node/Trigger_SwitchToggle.cs
+++ Client/Test/Assets/BehaviorTree/Editor/Node/Trigger_SwitchToggle.cs
@@ -5,11 +5,11 @@ using UnityEditor.Experimental.GraphView;
 public class Trigger_SwitchToggle : TriggerNode
 {
     public override string stateName => "SwitchToggleState";
-    public Trigger_SwitchToggle() : base() 
+    public Trigger_SwitchToggle() : base()
     {
         title = "SwitchToggle";
-        
-        
+
+
         Port port_exit = CreatePortForNode(this, Direction.Output, typeof(System.Boolean), Port.Capacity.Single);
         port_exit.portName = "exit";
         outputContainer.Add(port_exit);
--- Client/Test/Assets/BehaviorTree/Editor/Node/Trigger_ToggleGroup.cs
+++ Client/Test/Assets/BehaviorTree/Editor/Node/Trigger_ToggleGroup.cs
@@ -4,15 +4,15 @@ using UnityEditor.Experimental.GraphView;
 public class Trigger_ToggleGroup : TriggerNode
 {
     public override string stateName => "ToggleGroupState";
-    public Trigger_ToggleGroup() : base() 
+    public Trigger_ToggleGroup() : base()
     {
         title = "ToggleGroup";
-        
+
         Port port_enter = CreatePortForNode(this, Direction.Input, typeof(System.Boolean), Port.Capacity.Single);
         port_enter.portName = "enter";
         inputContainer.Add(port_enter);
 
-        
+
         Port port_exit = CreatePortForNode(this, Direction.Output, typeof(System.Boolean), Port.Capacity.Multi);
         port_exit.portName = "exit";
         outputContainer.Add(port_exit);
