668652f2900ddd58dbffd6738020e3e2707a32e1
duyutong <729052582@qq.com>
2026-04-09 09:34:47 +0800

    -

---
+++ Server/FlexiServer/Core/RandomGenerator.cs
@@ -0,0 +1,47 @@
+﻿namespace FlexiServer.Core
+{
+    public class RandomGenerator
+    {
+        private static Random random;
+        public static void InitRandom()
+        {
+            int seed = DateTime.Now.Millisecond;
+            random = new Random(seed);
+        }
+        public static int GetRandomInt(int min, int max)
+        {
+            if (random == null) InitRandom();
+            return random.Next(min, max);
+        }
+        public static int GetRandomInt(int max)
+        {
+            if (random == null) InitRandom();
+            return random.Next(max);
+        }
+        public static float GetRandomFloat(float min, float max)
+        {
+            if (random == null) InitRandom();
+            return (float)(random.NextDouble() * (max - min) + min);
+        }
+        public static float GetRandomFloat(float max)
+        {
+            if (random == null) InitRandom();
+            return (float)(random.NextDouble() * max);
+        }
+        public static double GetRandomDouble(double min, double max)
+        {
+            if (random == null) InitRandom();
+            return random.NextDouble() * (max - min) + min;
+        }
+        public static void GetRandomBytes(byte[] buffer)
+        {
+            if (random == null) InitRandom();
+            random.NextBytes(buffer);
+        }
+        public static void GetRandomBytes(Span<byte> buffer)
+        {
+            if (random == null) InitRandom();
+            random.NextBytes(buffer);
+        }
+    }
+}
