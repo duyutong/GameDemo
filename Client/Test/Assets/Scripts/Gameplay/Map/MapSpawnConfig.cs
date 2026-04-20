04b31633a75e4d026b11d4dafa3dac978dd3638c
duyutong <729052582@qq.com>
2026-04-20 09:25:55 +0800

    -

--- Client/Test/Assets/Scripts/Gameplay/Map/MapSpawnConfig.cs
+++ Client/Test/Assets/Scripts/Gameplay/Map/MapSpawnConfig.cs
@@ -11,4 +11,9 @@ public class MapSpawnConfig : ScriptableObject
     [Range(0, 1)] public float min = 0.1f;
     [Range(0, 1)] public float max = 0.5f;
     [MinMaxRangeSlider(0, 1)] public Vector2 probability = new Vector2(0.2f, 0.5f);
+
+    public void GenSpawnConfig() 
+    {
+
+    }
 }
